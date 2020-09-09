4e85e097ecfaa9436e89c3c8e60e8a124c6f2bef
Suphachai Hestmark <top_hestmark@yahoo.no>
2020-09-09 14:49:20 +0200

    Add a bit of styling to checkoutProduct

--- src/components/Reducer.js
+++ src/components/Reducer.js
@@ -1,5 +1,11 @@
 export const initialState = {
-  basket: [],
+  basket: [{
+    id: "P568R6BPYFE7507YAKC1",
+    title: "Lenovo Flex 5 14in 2-in-1 Laptop, 14.0in FHD (1920 x 1080) Touch Display, AMD Ryzen 5 4500U Processor, 16GB DDR4, 256GB SSD, AMD Radeon Graphics, Digital Pen Included, Win 10, 81X20005US, Graphite Grey",
+    rating: 5,
+    price: 599.99,
+    image: "https://images-na.ssl-images-amazon.com/images/I/81x%2B9uFY1QL._AC_SL1500_.jpg"
+  }],
   user: null,
 };
 
